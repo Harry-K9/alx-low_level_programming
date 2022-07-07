83b4936e7b5cedf063baa75b83b49e83044daead
Harry-K9 <kangetheharry72@gmail.com>
2022-07-07 05:56:58 -0700

    project

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
  * print_numbers - function that print all numbers from
- * 0 to 9
- *
+ * 0 to
  * Return: returns nothing
  */
+
 void print_numbers(void)
 {
 	int number = 48;
