824d86ea48bc9df494aa264a3796a3090a364fe0
Harry-K9 <kangetheharry72@gmail.com>
2022-07-07 07:34:41 -0700

    project

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,50 +1,35 @@
 #include "main.h"
 
 /**
- * print_number - print number
- * @n: input value
+ * print_number - prints # using _putchar function
+ * @n: the integer to print
  *
- * Description:  prints an integer
- * Return: Always (0)
+ * Return: void
  */
-
 void print_number(int n)
 {
-	if ((n < 0) && (n >= -9))
+	int copy, nth, size = 1, ones = n % 10;
+
+	n /= 10;
+	copy = n;
+	if (ones < 0)
 	{
+		ones *= -1, copy *= -1, n *= -1;
 		_putchar('-');
-		_putchar((n * -1) + '0');
 	}
-	else if (n <= -10)
+	if (copy > 0)
 	{
-		_putchar('-');
-		_putchar(((n / 10) * -1) + '0');
-		_putchar(((n % 10)  * -1) + '0');
+		while (copy / 10 != 0)
+		{
+			copy /= 10, size *= 10;
+		}
+		while (size > 0)
+		{
+			nth = n / size;
+			_putchar('0' + nth);
+			n -= nth * size;
+			size /= 10;
+		}
 	}
-	else if (n == 0)
-	{
-		_putchar(n + '0');
-	}
-	else if ((n > 0) && (n <= 9))
-	{
-		_putchar(n + '0');
-	}
-	else if ((n >= 10) && (n <= 99))
-	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if ((n >= 100) && (n <= 999))
-	{
-		_putchar((n / 100) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if ((n >= 1000) && (n <= 9999))
-	{
-		_putchar((n / 1000) + '0');
-		_putchar((n / 100) % 10 + '0');
-		_putchar((n / 10) % 10 + '0');
-		_putchar((n % 10) + '0');
-	}
+	_putchar('0' + ones);
 }
