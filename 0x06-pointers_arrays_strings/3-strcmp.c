6f16ed9afb4039ebc126e782ccc117af3c84041b
Harry-K9 <kangetheharry72@gmail.com>
2022-07-13 11:02:34 -0700

    project

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,16 +7,29 @@ nclude "main.h"
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i = 0, diff = 0;
 
-	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (1)
 	{
-		if (s1[i] != s2[i])
+		if (s1[i] == '\0' && s2[i] == '\0')
+			break;
+		else if (s1[i] == '\0')
 		{
-			return (s1[i] - s2[i]);
+			diff = s2[i];
+			break;
 		}
-		i++;
+		else if (s2[i] == '\0')
+		{
+			diff = s1[i];
+			break;
+		}
+		else if (s1[i] != s2[i])
+		{
+			diff = s1[i] - s2[i];
+			break;
+		}
+		else
+			i++;
 	}
-	return (0);
+	return (diff);
 }
